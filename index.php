cd22072f4edd30cf05de3f69f71f59cfcb1a880a
anton-zamniborshch <anton.zamniborshch@boosta.co>
2021-07-11 13:01:58 +0300

    Add composer autoload

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+$mcDuck = new \App\McDuck();
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3bf25b17084fc857bd005506482895ae54ee8810',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3bf25b17084fc857bd005506482895ae54ee8810',
+    ),
+  ),
+);
